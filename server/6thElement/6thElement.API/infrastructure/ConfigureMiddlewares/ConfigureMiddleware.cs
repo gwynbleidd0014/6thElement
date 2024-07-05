ea8fe8804629d2959c9c389b11e0855a63cf2e98
Ucha Omiadze <mr.nemo0014@gmail.com>
2024-07-05 19:23:00 +0400

    update static files

--- server/6thElement/6thElement.API/infrastructure/ConfigureMiddlewares/ConfigureMiddleware.cs
+++ server/6thElement/6thElement.API/infrastructure/ConfigureMiddlewares/ConfigureMiddleware.cs
@@ -15,11 +15,11 @@ public static class ConfigureMiddleware
     public static void UseConfiguredStaticFiles(this IApplicationBuilder builder, IWebHostEnvironment environment, IConfiguration config)
     {
         var RequestPath = string.Format("/{0}", config.GetValue<string>("Constants:ResourcePath"));
-
+        var absolutePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), config.GetValue<string>("Constants:UploadsFolderPath")));
 
         builder.UseStaticFiles(new StaticFileOptions
         {
-            FileProvider = new PhysicalFileProvider(config.GetValue<string>("Constants:UploadsFolderPath")),
+            FileProvider = new PhysicalFileProvider(absolutePath),
             RequestPath = RequestPath
         });
     }
